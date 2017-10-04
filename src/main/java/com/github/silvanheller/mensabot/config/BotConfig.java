421768ee5385e33b9c0eec920289a5bcc6f8fca3
Silvan Heller <silvan.heller@unibas.ch>
2017-10-04 13:04:24 +0200

    Adding botusername to config

--- src/main/java/com/github/silvanheller/mensabot/config/BotConfig.java
+++ src/main/java/com/github/silvanheller/mensabot/config/BotConfig.java
@@ -8,6 +8,6 @@ import com.typesafe.config.ConfigFactory;
 public class BotConfig {
 
     public static final String SECRET_TOKEN = ConfigFactory.load( "mensabot" ).getString( "mensa-bot.token" );
-    public static final String BOT_USERNAME = "UnibasMensaBot";
+    public static final String BOT_USERNAME = ConfigFactory.load( "mensabot" ).getString( "mensa-bot.botname" );
 
 }
